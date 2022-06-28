b597e4fca0d7b4b9f08aa81f5144b80b0df6e772
Kaxramonovravshan <r17ravshan@gmail.com>
2022-06-28 18:58:18 +0500

    first commit

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^s%w7@ejf5++f(x=j1h6r1^u*n)#k455&ju))rbxjdfay945ge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['qahramonov.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['qahramonovravshan.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
